22f632f4f67867f75d5474593c101d0a08b178a4
basicShaahid <shaahidbasic101@gmail.com>
2026-04-14 20:17:03 +1000

    cleaning project and rewriting code on own

---
+++ types/gem.ts
@@ -0,0 +1,12 @@
+export type Gem = {
+  slug: string;
+  name: string;
+  origin: string;
+  cut: string;
+  color: string;
+  carat: string;
+  price: string;
+  image: string;
+  description: string;
+  featured?: boolean;
+};
